02d53a7e94d96fcd353593f01092203a808d6168
salimza3a <salimsalimzada2002@gmail.com>
2022-05-16 13:57:09 +0400

    updated files

---
+++ assets/game.js
@@ -0,0 +1,40 @@
+$(document).ready( function() {
+    let getRandomNumber = Math.floor(Math.random() * 120)
+
+    $("#green-number").html(getRandomNumber)
+
+  
+    let counter = 0;
+    let wins = 0;
+    let losses = 0;
+
+    $("#wins").html(wins)
+    $("#losses").html(losses)
+    $("#score").html(counter)
+
+    $(".crystal-image").on("click", function () {
+        let crystalValue = $(this).attr("data-value")
+
+      
+        counter += +crystalValue
+
+        console.log(counter)
+
+        $("#score").html(counter)
+
+        if( counter === getRandomNumber) {
+            $("#text").html("You won!!")
+            wins += 1;
+            $("#wins").html(wins)
+            counter = 0;
+            $("#score").html(counter)
+
+        } else if(counter > getRandomNumber) {
+            $("#text").html("You lost!!")
+            losses += 1;
+            $("#losses").html(losses)
+            counter = 0;
+            $("#score").html(counter)
+        }
+    })
+})
